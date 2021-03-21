774b622b8ad9ab4e092739ef6fd1d29ecdf875aa
Roman <Melnikov_RV@list.ru>
2021-03-21 09:05:10 +0700

    homeWork-3

---
+++ task4.js
@@ -0,0 +1,3 @@
+"use strict"
+
+for (var i = 0; i < 10; console.log(i), i++);
---
+++ task5.js
@@ -0,0 +1,8 @@
+"use strict"
+
+for (var i = 1; i <= 20 ; i++) {
+    for (var j = 1, x = ""; j <= i; j++) {
+         x += "x";
+    }
+    console.log(x);
+}
